6aa8fee9e6431072f5e279351f2f4697f0347770
rizalrfsnz <rizalrafsanzani66@gmail.com>
2025-05-11 23:57:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('show'); 
+});
+
+
+const navLinks = document.querySelectorAll('#navbar a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const navbar = document.getElementById('navbar');
+        navbar.classList.remove('show'); 
+    });
+});
+
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active'); 
+});
